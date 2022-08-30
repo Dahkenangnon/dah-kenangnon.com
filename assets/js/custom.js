201208ed000b3c83f1bbf249422f7bcac84cd3f7
Dahkenangnon <dah.kenangnon@gmail.com>
2022-08-30 13:36:45 +0100

    Update: add random bg && realtime clock

---
+++ assets/js/custom.js
@@ -0,0 +1,88 @@
+/*----------------------------------------------------------------------------------
+                        (c) https://codepen.io/chrisota/pen/LERyjb
+------------------------------------------------------------------------------------*/
+
+window.requestAnimFrame = (function () {
+    return window.requestAnimationFrame ||
+        window.webkitRequestAnimationFrame ||
+        window.mozRequestAnimationFrame ||
+        function (callback) {
+            window.setTimeout(callback, 1000 / 60);
+        };
+})();
+
+//initialize the clock in a self-invoking function
+(function clock() {
+    var hour = document.getElementById("hour"),
+        min = document.getElementById("min"),
+        sec = document.getElementById("sec");
+    //set up a loop
+    (function loop() {
+        requestAnimFrame(loop);
+        draw();
+    })();
+    //position the hands
+    function draw() {
+        var now = new Date(),//now
+            then = new Date(now.getFullYear(), now.getMonth(), now.getDate(), 0, 0, 0),//midnight
+            diffInMil = (now.getTime() - then.getTime()),// difference in milliseconds
+            h = (diffInMil / (1000 * 60 * 60)),//hours
+            m = (h * 60),//minutes
+            s = (m * 60);//seconds
+        //rotate the hands accordingly
+        sec.style.webkitTransform = "rotate(" + (s * 6) + "deg)";
+        hour.style.webkitTransform = "rotate(" + (h * 30 + (h / 2)) + "deg)";
+        min.style.webkitTransform = "rotate(" + (m * 6) + "deg)";
+    }
+})();
+
+/*----------------------------------------------------------------------------------
+                        (c) Justin Dah-kenangnon
+------------------------------------------------------------------------------------*/
+function applyRandomBackground() {
+    let styles = [
+        " box-shadow: 14px 14px 0 -4px gold, 14px 14px 0 0 black;",
+        "box-shadow: rgba(0, 0, 0, 0.09) 0px 3px 12px;",
+        "box-shadow: rgba(0, 0, 0, 0.45) 0px 25px 20px -20px;",
+        "box-shadow: rgba(0, 0, 0, 0.15) 1.95px 1.95px 2.6px;",
+        "box-shadow: rgba(0, 0, 0, 0.35) 0px 5px 15px;",
+        "box-shadow: rgba(0, 0, 0, 0.25) 0px 54px 55px, rgba(0, 0, 0, 0.12) 0px -12px 30px, rgba(0, 0, 0, 0.12) 0px 4px 6px, rgba(0, 0, 0, 0.17) 0px 12px 13px, rgba(0, 0, 0, 0.09) 0px -3px 5px;",
+        "box-shadow: rgba(50, 50, 93, 0.25) 0px 50px 100px -20px, rgba(0, 0, 0, 0.3) 0px 30px 60px -30px, rgba(10, 37, 64, 0.35) 0px -2px 6px 0px inset;",
+        "box-shadow: rgb(38, 57, 77) 0px 20px 30px -10px;",
+        "box-shadow: rgba(6, 24, 44, 0.4) 0px 0px 0px 2px, rgba(6, 24, 44, 0.65) 0px 4px 6px -1px, rgba(255, 255, 255, 0.08) 0px 1px 0px inset;",
+        "box-shadow: rgba(0, 0, 0, 0.12) 0px 1px 3px, rgba(0, 0, 0, 0.24) 0px 1px 2px;",
+        "box-shadow: rgba(0, 0, 0, 0.17) 0px -23px 25px 0px inset, rgba(0, 0, 0, 0.15) 0px -36px 30px 0px inset, rgba(0, 0, 0, 0.1) 0px -79px 40px 0px inset, rgba(0, 0, 0, 0.06) 0px 2px 1px, rgba(0, 0, 0, 0.09) 0px 4px 2px, rgba(0, 0, 0, 0.09) 0px 8px 4px, rgba(0, 0, 0, 0.09) 0px 16px 8px, rgba(0, 0, 0, 0.09) 0px 32px 16px;",
+        "box-shadow: rgba(255, 255, 255, 0.2) 0px 0px 0px 1px inset, rgba(0, 0, 0, 0.9) 0px 0px 0px 1px;",
+        "box-shadow: rgba(0, 0, 0, 0.4) 0px 2px 4px, rgba(0, 0, 0, 0.3) 0px 7px 13px -3px, rgba(0, 0, 0, 0.2) 0px -3px 0px inset;",
+        "box-shadow: rgb(204, 219, 232) 3px 3px 6px 0px inset, rgba(255, 255, 255, 0.5) -3px -3px 6px 1px inset;",
+        "box-shadow: rgba(3, 102, 214, 0.3) 0px 0px 0px 3px;",
+        "box-shadow: rgba(240, 46, 170, 0.4) 0px 5px, rgba(240, 46, 170, 0.3) 0px 10px, rgba(240, 46, 170, 0.2) 0px 15px, rgba(240, 46, 170, 0.1) 0px 20px, rgba(240, 46, 170, 0.05) 0px 25px;",
+        "box-shadow: rgba(240, 46, 170, 0.4) -5px 5px, rgba(240, 46, 170, 0.3) -10px 10px, rgba(240, 46, 170, 0.2) -15px 15px, rgba(240, 46, 170, 0.1) -20px 20px, rgba(240, 46, 170, 0.05) -25px 25px;",
+        "box-shadow: rgba(240, 46, 170, 0.4) 5px 5px, rgba(240, 46, 170, 0.3) 10px 10px, rgba(240, 46, 170, 0.2) 15px 15px, rgba(240, 46, 170, 0.1) 20px 20px, rgba(240, 46, 170, 0.05) 25px 25px;",
+        "box-shadow: rgba(0, 0, 0, 0.09) 0px 2px 1px, rgba(0, 0, 0, 0.09) 0px 4px 2px, rgba(0, 0, 0, 0.09) 0px 8px 4px, rgba(0, 0, 0, 0.09) 0px 16px 8px, rgba(0, 0, 0, 0.09) 0px 32px 16px;",
+        "box-shadow: rgba(0, 0, 0, 0.06) 0px 2px 4px 0px inset;",
+        "box-shadow: rgba(60, 64, 67, 0.3) 0px 1px 2px 0px, rgba(60, 64, 67, 0.15) 0px 2px 6px 2px;",
+        "box-shadow: rgba(50, 50, 93, 0.25) 0px 30px 60px -12px inset, rgba(0, 0, 0, 0.3) 0px 18px 36px -18px inset;",
+        "box-shadow: blue 0px 0px 0px 2px inset, rgb(255, 255, 255) 10px -10px 0px -3px, rgb(31, 193, 27) 10px -10px, rgb(255, 255, 255) 20px -20px 0px -3px, rgb(255, 217, 19) 20px -20px, rgb(255, 255, 255) 30px -30px 0px -3px, rgb(255, 156, 85) 30px -30px, rgb(255, 255, 255) 40px -40px 0px -3px, rgb(255, 85, 85) 40px -40px;", "box-shadow: rgb(85, 91, 255) 0px 0px 0px 3px, rgb(31, 193, 27) 0px 0px 0px 6px, rgb(255, 217, 19) 0px 0px 0px 9px, rgb(255, 156, 85) 0px 0px 0px 12px, rgb(255, 85, 85) 0px 0px 0px 15px;", "box-shadow: rgb(85, 91, 255) 0px 0px 0px 3px, rgb(31, 193, 27) 0px 0px 0px 6px, rgb(255, 217, 19) 0px 0px 0px 9px, rgb(255, 156, 85) 0px 0px 0px 12px, rgb(255, 85, 85) 0px 0px 0px 15px;", "box-shadow: blue 0px 0px 0px 2px inset, rgb(255, 255, 255) 10px -10px 0px -3px, rgb(31, 193, 27) 10px -10px, rgb(255, 255, 255) 20px -20px 0px -3px, rgb(255, 217, 19) 20px -20px, rgb(255, 255, 255) 30px -30px 0px -3px, rgb(255, 156, 85) 30px -30px, rgb(255, 255, 255) 40px -40px 0px -3px, rgb(255, 85, 85) 40px -40px;"
+    ];
+
+    const chooseRandBoxShadow = () => {
+        return Math.floor(Math.random() * styles.length);
+
+    };
+
+    let bg = styles[chooseRandBoxShadow()];
+
+    var elements = document.querySelectorAll('.status-bg'), i;
+    for (i = 0; i < elements.length; ++i) {
+        elements[i].style = bg
+    }
+}
+applyRandomBackground();
+
+/*-------------------------
+	Other 
+
+    Neumorphism UI from: https://themesberg.com | https://www.themesberg.com/licensing#mit)
+    
+--------------------------*/
