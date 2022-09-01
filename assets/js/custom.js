6369694afaad0a68c785fe7429ba185c70fb4cdb
Dahkenangnon <dah.kenangnon@gmail.com>
2022-09-01 13:19:23 +0100

    Add humanity widget

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -80,6 +80,16 @@ function applyRandomBackground() {
 }
 applyRandomBackground();
 
+
+// C
+
+// var now = moment(new Date()); //todays date
+// var end = moment("2015-12-1"); // another date
+// var duration = moment.duration(now.diff(end));
+// var days = duration.asDays();
+// console.log(days)
+
+
 /*-------------------------
 	Other 
 
